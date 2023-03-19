7db44bf3c99aa836d58333e6358ae58af4b5bf1a
kiran dwivedi <kirandwivedi530gmail.com>
2023-03-19 05:38:38 +0000

    Technology Cards Test

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import './App.css'
+import CardItem from './components/CardItem'
+
+const cardsList = [
+  {
+    id: 1,
+    title: 'Data Scientist',
+    description:
+      'Data scientists gather and analyze large sets of structured and unstructured data',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/data-scientist-img.png',
+    className: 'card-1',
+  },
+  {
+    id: 2,
+    title: 'IOT Developer',
+    description:
+      'IoT Developers are professionals who can develop, manage, and monitor IoT devices.',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/iot-developer-img.png',
+    className: 'card-2',
+  },
+  {
+    id: 3,
+    title: 'VR Developer',
+    description:
+      'A VR developer creates completely new digital environments that people can see.',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/vr-developer-img.png',
+    className: 'card-3',
+  },
+  {
+    id: 4,
+    title: 'ML Engineer',
+    description:
+      'Machine learning engineers feed data into models defined by data scientists.',
+    imgUrl: 'https://assets.ccbp.in/frontend/react-js/ml-engineer-img.png',
+    className: 'card-4',
+  },
+]
+
+const App = () => (
+  <div className="list-container">
+    <h1 className="title">Learn 4.0 Technologies</h1>
+    <p className="top-description">
+      Get trained by alumni of IITs and top companies
+      Amazon,Intel,Microsoft.Learn directly from professional involved in
+      product development.
+    </p>
+    <ul>
+      {cardsList.map(eachItem => (
+        <CardItem cardDetails={eachItem} key={eachItem.id} />
+      ))}
+    </ul>
+  </div>
+)
+export default App
